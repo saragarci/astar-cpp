0231a49185756e1a30f4fa6691a2f788d35ffef9
saragarci <s@saragarci.com>
2021-09-30 14:16:04 +0200

    [IMP] clean up

--- src/main.cpp
+++ src/main.cpp
@@ -18,26 +18,23 @@ int main() {
     std::array<int, 2> est{1,0};
     std::array<std::array<int, 2>, 4> actions{north, south, west, est};
 
-    /* Task 0 */
+    // Task 0
     //Map map_task0{"../src/maps/map_1.csv", kCellSize};
     //std::array<int, 2> start_task0{0,0};
     //std::array<int, 2> goal_task0{9,4};
     //AstarSearch astar_task0(map_task0, start_task0, goal_task0, actions);
-    //astar_task0.showShortestPath();
 
-    /* Task 1 */
+    // Task 1
     //Map map_task1{"../src/maps/Samfundet_map_1.csv", kCellSize};
     //std::array<int, 2> start_task1{18,27};
     //std::array<int, 2> goal_task1{32,40};   
     //AstarSearch astar_task1(map_task1, start_task1, goal_task1, actions);
-    //astar_task1.showShortestPath();
 
     // Task 2
     //Map map_task2{"../src/maps/Samfundet_map_1.csv", kCellSize};
     //std::array<int, 2> start_task2{32,40};
     //std::array<int, 2> goal_task2{5,8};     
     //AstarSearch astar_task2(map_task2, start_task2, goal_task2, actions);
-    //astar_task2.showShortestPath();
 
     // Task 3
     Map map_task3{"../src/maps/Samfundet_map_2.csv", kCellSize};
@@ -51,6 +48,15 @@ int main() {
     //std::array<int, 2> goal_task4{32,6}; 
     //AstarSearch astar_task4(map_task4, start_task4, goal_task4, actions);
     
+    // Task 5
+    /*
+    elif task == 5:
+        start_pos = [14, 18]
+        goal_pos = [6, 36]
+        end_goal_pos = [6, 7]
+        path_to_map = 'Samfundet_map_2.csv'
+    */
+
     SDL_Event e;
     // Get user input to terminate the program
     while (!(e.type == SDL_QUIT)) {
@@ -60,12 +66,4 @@ int main() {
         else
             map_task3.print();
     }
-
-    /*
-    elif task == 5:
-        start_pos = [14, 18]
-        goal_pos = [6, 36]
-        end_goal_pos = [6, 7]
-        path_to_map = 'Samfundet_map_2.csv'
-    */
 }
